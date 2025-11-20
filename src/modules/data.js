ed893cccce4870b5e739a08ef5edb54e5f8d076b
elifyilmaz00 <elifyilmaz1906@gmail.com>
2025-11-20 19:36:20 +0300

    feat(setup): Initial project structure and base data module

---
+++ src/modules/data.js
@@ -0,0 +1,31 @@
+//Başlangıç reçete verileri.
+//Normalde bu bir API'den veya veritabanından alınır.
+
+export const initialRecipes = [
+    {
+
+    id: 1,
+    title: "Mercimek Çorbası",
+    ingredients: [
+        "1 su bardağı kırmızı mercimek",
+        "1 adet soğan",
+        "1 yemek kaşığı salça",
+        "6 su bardağı su",
+        "tuz, nane, pul biber"
+    ], 
+
+    insturctions: "Soğanı kavurun, salçayı ekleyin. Mercimeği ve suyu ekleyip pişirin. Baharatları ekleyip blendere'dan geçirin.",
+    image: "https://i.nefisyemektarifleri.com/2018/01/15/mercimek-corbasi-3.jpg"
+},
+
+{
+    id: 2,
+    title: "Menemen",
+    ingredients: ["3 adet domates", "2 adet yeşil biber", "2 adet yumurta", "Tuz, karabiber"],
+    insturctions: "Biberleri ve domatesleri doğrayıp pişirin. Yumurtaları kırıp karıştırarak pişirin. Baharatları ekleyin.",
+    image: "https://i.nefisyemektarifleri.com/2018/07/09/menemen-tarifi-fotografli.jpg"
+
+}
+
+];
+
